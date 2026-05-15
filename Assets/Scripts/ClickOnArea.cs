cb81bf5c57469b9ac5a553d2a1237d671b6e0cd3
mdl22 <131698082+mdl22@users.noreply.github.com>
2026-05-15 18:59:42 +0100

    ClickOnArea updated for "show"

--- Assets/Scripts/ClickOnArea.cs
+++ Assets/Scripts/ClickOnArea.cs
@@ -9,11 +9,14 @@ using TMPro;
 public class ClickOnArea : MonoBehaviour
 {
     [SerializeField] GameObject modelButtons;
+    [SerializeField] GameObject areaButtons;
 
     [SerializeField] Button controlsExitButton;
     [SerializeField] Button resetButton;
     [SerializeField] Button areasExitButton;
     [SerializeField] Button backButton;
+    [SerializeField] Button showButton;
+    [SerializeField] Button findButton;
 
     [SerializeField] Image areasPanel;
 
@@ -36,6 +39,7 @@ public class ClickOnArea : MonoBehaviour
     int bitPosition;            // starting from the most significant bit in bit string
     float elapsedTime;
     string bitString = "";
+    bool inShowMode;
 
     void Start()
     {
@@ -44,25 +48,27 @@ public class ClickOnArea : MonoBehaviour
 
         SetUpEmissionMaps();
 
-        controlsExitButton.GetComponent<Button>().onClick.AddListener(() =>
-            { SetEmissionColor(0, true); });
-        resetButton.GetComponent<Button>().onClick.AddListener(() =>
-            { SetEmissionColor(0, true); });
-        areasExitButton.GetComponent<Button>().onClick.AddListener(() =>
-            { SetEmissionColor(0, true); });
-        backButton.GetComponent<Button>().onClick.AddListener(() =>
-            { SetEmissionColor(0, true); });
-
-        foreach (Button button in modelButtons.GetComponentsInChildren<Button>())
+        List<Button> buttonList = new List<Button> {controlsExitButton, resetButton,
+            areasExitButton, backButton, showButton, findButton};
+        buttonList.AddRange(modelButtons.GetComponentsInChildren<Button>());
+        foreach (Button button in buttonList)
         {
             button.onClick.AddListener(() => { SetEmissionColor(0, true); });
         }
+
+        showButton.GetComponent<Button>().onClick.AddListener(ActivateAreaButtons);
+        foreach (Button button in areaButtons.GetComponentsInChildren<Button>(true))
+        {
+            button.onClick.AddListener(() => { AreaButtonFunctionality(button); });
+        }
+
+        bitString = "";
     }
 
     void Update()
     {
         if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began
-            && areasPanel.gameObject.activeSelf)
+            && areasPanel.gameObject.activeSelf && !inShowMode)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             bool isNotOverUI = !EventSystem.current.IsPointerOverGameObject();
@@ -71,7 +77,31 @@ public class ClickOnArea : MonoBehaviour
                 Color32 pixelColour = mask.GetPixel((int) (hit.textureCoord.x * mask.width),
                     (int) (hit.textureCoord.y * mask.height));
 
-                bitString = Convert.ToString(pixelColour.g, 2);     // removes leading zeroes
+                     // Convert.ToString() removes leading zeroes
+                     string rawBitString = Convert.ToString(pixelColour.g, 2);
+
+                bitString = "";
+                for (int bit = 0; bit < rawBitString.Length; bit++)
+                {
+                    int bitPosition = rawBitString.Length - bit;
+
+                    if (bitPosition > maps.Count)
+                    {
+                        bitString += "0";
+                    }
+                    else
+                    {
+                        if (maps[(1 << bitPosition - 1).ToString()] == null ||
+                            !areas.ContainsKey((1 << bitPosition - 1).ToString()))
+                        {
+                            bitString += "0";
+                        }
+                        else
+                        {
+                            bitString += rawBitString[bit];
+                        }
+                    }
+                }
 
                 if (bitString.Split('1').Length == 1)   // no emission map
                 {
@@ -83,16 +113,13 @@ public class ClickOnArea : MonoBehaviour
                 {
                     string areasKey = (1 << bitString.Length - 1).ToString();
 
-                    if (areas.ContainsKey(areasKey))
-                    {
-                        material.SetTexture("_EmissionMap", maps[areasKey]);
-                        SetEmissionColor(emissionIntensity);
-            
-                        areaTitleText.text = areas[areasKey][0];
-                        areaDescriptionText.text = areas[areasKey][1];
+                    material.SetTexture("_EmissionMap", maps[areasKey]);
+                    SetEmissionColor(emissionIntensity);
 
-                        GetComponentInParent<UIManager>().ResetAreasPanel(false);
-                    }
+                    areaTitleText.text = areas[areasKey][0];
+                    areaDescriptionText.text = areas[areasKey][1];
+
+                    GetComponentInParent<UIManager>().ResetAreasPanel(false);
                 }
             }
         }
@@ -104,10 +131,9 @@ public class ClickOnArea : MonoBehaviour
                 if (bitString[bit] == '1')
                 {
                     string areasKey = (1 << bitString.Length - 1).ToString();
+                    string mapKey = (1 << bitString.Length - 1 - bit).ToString();
 
-                    material.SetTexture("_EmissionMap",
-                        maps[(1 << bitString.Length - 1 - bit).ToString()]);
-                    SetEmissionColor(bit == 0 ? (byte) emissionIntensity : (byte) 0x7F);
+                    material.SetTexture("_EmissionMap", maps[mapKey]);
                     if (bit == 0)
                     {
                         SetEmissionColor((byte) emissionIntensity);
@@ -148,18 +174,19 @@ public class ClickOnArea : MonoBehaviour
         foreach (string line in maskTable.text.Split("\n"))
         {
             string[] fields = line.Split("\t");
-            if (line.Length > 0 && Char.IsDigit(fields[0][0]))  // ignore EOF and header
+            
+            if (line.Length > 0 && Char.IsDigit(fields[0][0]))  // ignore EOF, header
             {
-                areas.Add(fields[0],            // value
-                    new string[] {fields[2],    // name
-                                  fields[3],    // description
-                                  fields[4]});  // parent region
-                maps.Add(fields[0], emissionMaps[maps.Count]);
-
-                if (Char.IsLetter(fields[2][0]))
+                if (Char.IsLetter(fields[2][0]))    // ignore blank fields
                 {
+                    areas.Add(fields[0],            // value
+                        new string[] {fields[2],    // name
+                                      fields[3],    // description
+                                      fields[4]});  // parent region
                     panelListText.text += string.Concat(fields[2], "\n\n");
                 }
+
+                maps.Add(fields[0], emissionMaps[maps.Count]);
             }
         }
     }
@@ -171,5 +198,52 @@ public class ClickOnArea : MonoBehaviour
             bitString = "";
         }
         material.SetColor("_EmissionColor", new Color32(intensity, 0, 0, 0));
+
+        inShowMode = false;
+    }
+
+    void ActivateAreaButtons()
+    {
+        Button[] buttonArray = areaButtons.GetComponentsInChildren<Button>(true);
+
+        int i = 0;
+        foreach (string area in panelListText.text.Remove(
+            panelListText.text.Length - 2).Split("\n\n"))
+        {
+            buttonArray[i].GetComponentInChildren<TextMeshProUGUI>().text =
+                panelListText.text.Split("\n\n")[i];
+            buttonArray[i].GetComponentInChildren<TextMeshProUGUI>().fontSize =
+                buttonArray[i].GetComponentInChildren<TextMeshProUGUI>().text.Length > 15 ?
+                14.4f : 18;
+
+            buttonArray[i++].gameObject.SetActive(true);
+        }
+    }
+
+    void AreaButtonFunctionality(Button clicked)
+    {
+        foreach (Button button in areaButtons.GetComponentsInChildren<Button>())
+        {
+            button.interactable = clicked.name != button.name;
+        }
+
+        foreach (string areasKey in areas.Keys)
+        {//Debug.Log((areas.Count, areas.Keys.Count, areasKey));
+            if (areas[areasKey][0] == clicked.GetComponentInChildren<TextMeshProUGUI>().text)
+            {//Debug.Log(clicked.GetComponentInChildren<TextMeshProUGUI>().text);
+                material.SetTexture("_EmissionMap", maps[areasKey]);
+                SetEmissionColor(emissionIntensity);
+
+                areaTitleText.text = areas[areasKey][0];
+                areaDescriptionText.text = areas[areasKey][2] == "N/A" ? areas[areasKey][1] :
+                    string.Concat(areas[areasKey][1], "\n\nParent region: ",
+                    areas[areasKey][2].ToLower());
+
+                GetComponentInParent<UIManager>().ResetAreasPanel(false);
+
+                inShowMode = true;      // set to false in SetEmissionColor
+                break;
+            }
+        }
     }
 }
